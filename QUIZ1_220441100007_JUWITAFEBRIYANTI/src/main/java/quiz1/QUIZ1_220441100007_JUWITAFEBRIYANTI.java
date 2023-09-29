ee9c0a68b7eabe8543dfa6d775903247787105de
juwitafebriyanti <113221680+juwitafebriyanti@users.noreply.github.com>
2023-09-29 20:29:00 +0700

    Add files via upload

---
+++ QUIZ1_220441100007_JUWITAFEBRIYANTI/src/main/java/quiz1/QUIZ1_220441100007_JUWITAFEBRIYANTI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package quiz1;
+
+/**
+ *
+ * @author juwit
+ */
+public class QUIZ1_220441100007_JUWITAFEBRIYANTI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
